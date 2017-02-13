13f5213e2cfbe38c657e19007ed0313db1d43d62
0x1CEB00DA <github-iceboodha+spam@corezero.net>
2017-02-14 01:32:50 +0200

    make scanner return (Vec<String>, Vec<Error>)

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,21 @@ use dedupers::scan::scan;
 
 
 fn main() {
-    let scan_result = scan(&["/tmp"]);
-    match scan_result {
-        Ok(files) => 
-            for file in files {
-                println!("{:?}", file);
-            },
-        Err(err) => println!("ERROR: {}", err),
+    let scan_result = scan(&["/etc"]);
+    // match scan_result {
+    //     Ok(files) => 
+    //         for file in files {
+    //             println!("{:?}", file);
+    //         },
+    //     Err(err) => println!("ERROR: {}", err),
+    // }
+    let (files, errors) = scan_result;
+    for file in files {
+        println!("{:?}", file);
     }
+    println!("ERRORS:");
+    for error in errors {
+        println!("{:?}", error);
+    }
+
 }
--- src/scan.rs
+++ src/scan.rs
@@ -1,20 +1,25 @@
 use walkdir::{WalkDir,WalkDirIterator,DirEntry,Error};
 
-pub fn scan(dirs: &[&str]) -> Result<Vec<String>, Error> {
+pub fn scan(dirs: &[&str]) -> (Vec<String>, Vec<Error>) {
     scan_and_filter(dirs, |_| { true })
 }
 
-pub fn scan_and_filter<P>(dirs: &[&str], filter: P) -> Result<Vec<String>, Error>
+pub fn scan_and_filter<P>(dirs: &[&str], filter: P) -> (Vec<String>, Vec<Error>)
     where P: Fn(&DirEntry) -> bool {
     let mut files = Vec::<String>::new();
+    let mut errors = Vec::<Error>::new();
     for dir in dirs {
         // This should be passed in as an argument to allow the function to be tested
         let walker = WalkDir::new(dir).into_iter().filter_entry(&filter);
         for entry in walker {
-            let real_entry = entry?;
-            let file_name = real_entry.path().display().to_string();
-            files.push(file_name);
+            match entry {
+                Ok(file) => {
+                    let file_name = file.path().display().to_string();
+                    files.push(file_name)
+                },
+                Err(error) => errors.push(error)
+            }
         }
     }
-    Ok(files)
+    (files, errors)
 }
